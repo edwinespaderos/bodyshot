55b2c93841a7552243700be279cecac48b7a1a60
Edwin Espaderos <eespaderos@gmail.com>
2014-12-17 21:23:52 -0500

    updated slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,128 +1,6 @@
 $(document).ready(function() {
   
 
-  $(".next").on("click", function(e){
-      
-      var currentActiveSection = $(".image-shown");
-      var nextActiveSection = currentActiveSection.next();
-       
-        if(nextActiveSection.length == 0){
-          nextActiveSection = $(".slider div").first();
-        };
-
-
-      currentActiveSection.removeClass('image-shown').addClass('image-hidden')//.css("z-index", -10);
-      nextActiveSection.addClass('image-shown').removeClass('image-hidden');
-
-      $(".slider img").not([currentActiveSection, nextActiveSection])//.css("z-index", 1);
-      
-      e.preventDefault();
-
-  });
-
-
-  $(".previous").on("click", function(e){
-
-    var currentActiveSection = $(".image-shown");
-    var prevActiveSection = currentActiveSection.prev();
-
-      if (prevActiveSection.length == 0){
-        prevActiveSection = $(".slider div").first();
-      };
-
-    currentActiveSection.removeClass('image-shown').addClass('image-hidden')//.css("z-index", 10);
-    prevActiveSection.addClass('image-shown').removeClass('image-hidden');
-
-    $(".slider img").not([currentActiveSection, prevActiveSection])//.css("z-index", 1);
-
-    e.preventDefault();
-
-  });
- 
-  $(".menu-rum").on("click", function(){
-    
-      $(".rum").removeClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").addClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").addClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").addClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").addClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").addClass('image-hidden')//.css("z-index", -10);
-      $(".trance").addClass('image-hidden')//.css("z-index", -10);
-    
-    });
-  
-  $(".menu-menthol").on("click", function(){
-      
-      $(".rum").addClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").removeClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").addClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").addClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").addClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").addClass('image-hidden')//.css("z-index", -10);
-      $(".trance").addClass('image-hidden')//.css("z-index", -10);
-    
-    });
-
-  $(".menu-mojito").on("click", function(){
-        
-      $(".rum").addClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").addClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").removeClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").addClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").addClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").addClass('image-hidden')//.css("z-index", -10);
-      $(".trance").addClass('image-hidden')//.css("z-index", -10);
-    
-    });
-
-  $(".menu-tobacco").on("click", function(){
-            
-      $(".rum").addClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").addClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").addClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").removeClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").addClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").addClass('image-hidden')//.css("z-index", -10);
-      $(".trance").addClass('image-hidden')//.css("z-index", -10);
-    
-    });
-
-  $(".menu-sangria").on("click", function(){
-            
-      $(".rum").addClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").addClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").addClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").addClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").removeClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").addClass('image-hidden')//.css("z-index", -10);
-      $(".trance").addClass('image-hidden')//.css("z-index", -10);
-    
-    });
-
-$(".menu-whiskey").on("click", function(){
-            
-      $(".rum").addClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").addClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").addClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").addClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").addClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").removeClass('image-hidden')//.css("z-index", -10);
-      $(".trance").addClass('image-hidden')//.css("z-index", -10);
-    
-    });
-
-$(".menu-trance").on("click", function(){
-            
-      $(".rum").addClass('image-hidden')//.css("z-index", -10);
-      $(".menthol").addClass('image-hidden')//.css("z-index", -10);
-      $(".mojito").addClass('image-hidden')//.css("z-index", -10);
-      $(".tobacco").addClass('image-hidden')//.css("z-index", -10);
-      $(".sangria").addClass('image-hidden')//.css("z-index", -10);
-      $(".whiskey").addClass('image-hidden')//.css("z-index", -10);
-      $(".trance").removeClass('image-hidden')//.css("z-index", -10);
-    
-    });
-
 $('#btn-confirm').click(function(){
     $("#age-verefier").addClass("active");
 });
@@ -157,5 +35,82 @@ $('#btn-confirm').click(function(){
   });
 
 
+  $(function() {
+      $('.banner').unslider({
+        speed: 400,               //  The speed to animate each slide (in milliseconds)
+        delay: 3000,              //  The delay between slide animations (in milliseconds)
+        complete: function() {},  //  A function that gets called after every slide animation
+        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+        dots: false,               //  Display dot navigation
+        fluid: true              //  Support responsive design. May break non-responsive designs
+      });
+  });
+
+  var unslider = $('.banner').unslider();
+      
+      $('.next').click(function(e) {
+          var fn = this.className.split(' ')[1];
+          e.preventDefault();
+          //  Either do unslider.data('unslider').next() or .prev() depending on the className
+          unslider.data('unslider').next();
+      });
+
+  var unslider = $('.banner').unslider();
+      
+      $('.previous').click(function(e) {
+          var fn = this.className.split(' ')[1];
+          e.preventDefault();
+          //  Either do unslider.data('unslider').next() or .prev() depending on the className
+          unslider.data('unslider').prev();
+      });
+
+ 
+  var unslider = $('.banner').unslider();
+        
+        $('.menu-rum').click(function(e) {
+            
+            unslider.data('unslider').move(0);
+        });
+  
+  var unslider = $('.banner').unslider();
+  
+  $('.menu-menthol').click(function(e) {
+      
+      unslider.data('unslider').move(1);
+  });
+  
+  var unslider = $('.banner').unslider();
+        
+        $('.menu-tobacco').click(function(e) {
+            
+            unslider.data('unslider').move(2);
+        });
+
+  var unslider = $('.banner').unslider();
+        
+        $('.menu-sangria').click(function(e) {
+            
+            unslider.data('unslider').move(3);
+        });
+
+  var unslider = $('.banner').unslider();
+        
+        $('.menu-whiskey').click(function(e) {
+            
+            unslider.data('unslider').move(4);
+        });
+
+  var unslider = $('.banner').unslider();
+        
+        $('.menu-mojito').click(function(e) {
+            
+            unslider.data('unslider').move(5);
+        });
+
+  var unslider = $('.banner').unslider();
+        
+        $('.menu-trance').click(function(e) {
+            unslider.data('unslider').move(6);
+        });
 
 });
